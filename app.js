ce8ba267ddcbc4e5b4963b080b4d44c906e2598a
nickbradley <nickbradley@live.ca>
2016-06-19 13:14:11 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+express = require('express');
+var app = express();
+var exports = modules.exports = {};
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Magic is happening on port 3000');
+});
+
+exports.closeServer = function() {
+  server.close();
+};
